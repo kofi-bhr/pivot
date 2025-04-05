0813dbef64d4090012558fd22fa7f2e2f6e9bd10
Kofi Hair-Ralston <kofibhairralston@gmail.com>
2025-04-05 17:46:45 -0400

    discord -> signup

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -11,7 +11,7 @@ interface NavLink {
 const leftNavigation: NavLink[] = [
   { name: 'Home', href: '/' },
   { name: 'Articles', href: '/articles' },
-  { name: 'Discord', href: 'https://discord.gg/avYcgrJ6pA', external: true },
+  { name: 'Sign Up', href: 'https://forms.gle/VHDtEzJgNPLGNZiL9', external: true },
 ];
 
 const rightNavigation: NavLink[] = [
