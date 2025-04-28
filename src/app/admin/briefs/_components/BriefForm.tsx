f9a63428b8c5df57d379a5e92d6aab425cc0b36e
Kofi Hair-Ralston <kofibhairralston@gmail.com>
2025-04-28 13:58:21 -0400

    blah blah blah github

--- src/app/admin/briefs/_components/BriefForm.tsx
+++ src/app/admin/briefs/_components/BriefForm.tsx
@@ -1,3 +1,5 @@
+"use client";
+
 import { useRouter } from "next/navigation";
 import { FormEvent, useState } from "react";
 import { createClientComponentClient } from "@supabase/auth-helpers-nextjs";
